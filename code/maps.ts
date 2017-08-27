6f987f793c39d0d380caa72084352e0316864ae9
Abdullah Alhazmy <me@alhazmy13.net>
2017-08-27 10:14:31 +0300

    update map ts file

--- code/maps.ts
+++ code/maps.ts
@@ -1,5 +1,4 @@
-var occupations: Map<string, string> = new Map<string, string>();
+var occupations:{ [key: string]: string; } = {}
 occupations["Malcolm"] = "Captain";
 occupations["Kaylee"] = "Mechanic";
 occupations["Jayne"] = "Public Relations";
-console.log(occupations['Malcolm']);
