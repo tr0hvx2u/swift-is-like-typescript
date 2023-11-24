ec22c4f76630cba7b12fbf627db77639daea7e7d
Sergei Mirasov <118269956+tr0hvx2u@users.noreply.github.com>
2023-11-24 17:09:21 +0800

    Update range-operator.ts

--- code/range-operator.ts
+++ code/range-operator.ts
@@ -1,6 +1,6 @@
 let names = ["Anna", "Alex", "Brian", "Jack"];
 names.forEach((value: string, index: number) => {
-    console.log("Person " + index + " is called " + value);
+    console.log(`Person ${index + 1} is called ${value}`);
 });
 // Person 1 is called Anna
 // Person 2 is called Alex
